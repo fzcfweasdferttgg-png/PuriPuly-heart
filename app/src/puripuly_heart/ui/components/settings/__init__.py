050e0b05a98a6616ad4a047e950d3085e6238066
fzcfweasdferttgg-png <fzcfweasdferttgg@users.noreply.github.com>
2026-07-31 15:13:17 +0300

    cleanup: remove dead UI components (SettingsSection, ProviderSelector)

--- app/src/puripuly_heart/ui/components/settings/__init__.py
+++ app/src/puripuly_heart/ui/components/settings/__init__.py
@@ -6,9 +6,7 @@ from puripuly_heart.ui.components.settings.custom_vocabulary_tag_editor import (
     CustomVocabularyTagEditor,
 )
 from puripuly_heart.ui.components.settings.prompt_editor import PromptEditor
-from puripuly_heart.ui.components.settings.provider_selector import ProviderSelector
 from puripuly_heart.ui.components.settings.settings_modal import OptionItem, SettingsModal
-from puripuly_heart.ui.components.settings.settings_section import SettingsSection
 from puripuly_heart.ui.components.settings.settings_unit_card import SettingsUnitCard
 
 __all__ = [
@@ -17,8 +15,6 @@ __all__ = [
     "CustomVocabularyTagEditor",
     "OptionItem",
     "PromptEditor",
-    "ProviderSelector",
     "SettingsModal",
-    "SettingsSection",
     "SettingsUnitCard",
 ]
