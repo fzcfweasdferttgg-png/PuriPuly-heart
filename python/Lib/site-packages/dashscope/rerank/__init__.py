62c24a6fe6170dfc35440b7ee8308e3ebe0ae455
fzcfweasdferttgg-png <fzcfweasdferttgg@users.noreply.github.com>
2026-08-02 22:59:47 +0300

    chore: remove dead packages from portable Python (dashscope, google-genai, grpcio, etc.)

--- python/Lib/site-packages/dashscope/rerank/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) Alibaba, Inc. and its affiliates.
-
-from dashscope.rerank.text_rerank import AioTextReRank, TextReRank
-
-__all__ = ["AioTextReRank", "TextReRank"]
